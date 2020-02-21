b0d7574ba947aafa68093c714399ddcfe3b6ff0a
micmcgrorty <37194621+micmcgrorty@users.noreply.github.com>
2020-02-21 09:49:49 +0000

    initial commit

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	[SerializeField] float backgroundScrollSpeed = 0.5f;
+	Material backgroundMaterial;
+	Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+		backgroundMaterial = GetComponent<Renderer>().material;
+		offset = new Vector2(0f, backgroundScrollSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		backgroundMaterial.mainTextureOffset += offset * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[Header("Enemy Stats")]
+	[SerializeField] float health = 100f;
+	[SerializeField] int scoreValue = 150;
+
+	[Header("Shooting")]
+	float shotCounter;
+	[SerializeField] float minTimeBetweenShots = 0.2f;
+	[SerializeField] float maxTimeBetweenShots = 3f;
+	[SerializeField] GameObject laserPrefab;
+	[SerializeField] float projectileSpeed = 10f;
+
+	[Header("Sound Effects")]
+	[SerializeField] GameObject explosionEffect;
+	[SerializeField] float explosionTime = 1f;
+	[SerializeField] AudioClip deathSFX;
+	[SerializeField] [Range(0, 1)] float deathSoundVolume = 0.7f;
+	[SerializeField] AudioClip shootSFX;
+	[SerializeField] [Range(0, 1)] float shootSoundVolume = 0.7f;
+
+	// Use this for initialization
+	void Start () {
+		shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CountDownAndShoot();
+	}
+
+	private void CountDownAndShoot() {
+		shotCounter -= Time.deltaTime;
+		if (shotCounter <= 0f) {
+			Fire();
+			shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+		}
+	}
+
+	private void Fire() {
+		GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+		AudioSource.PlayClipAtPoint(shootSFX, Camera.main.transform.position, shootSoundVolume);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+		if (!damageDealer) {
+            return;
+        }
+		ProcessHit(damageDealer);
+	}
+
+	private void ProcessHit(DamageDealer damageDealer) {
+		health -= damageDealer.GetDamage();
+		damageDealer.Hit();
+
+		if (health <= 0) {
+			Die();
+		}
+	}
+
+	private void Die() {
+		FindObjectOfType<GameSession>().AddToScore(scoreValue);
+		Destroy(gameObject);
+		GameObject explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+		Destroy(explosion, explosionTime);
+		AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSoundVolume);
+	}
+}
